0b11dac7701794bb634dff2e3b059e59f14dec6a
byLLIPyT <alex-tt@mail.ru>
2019-12-16 22:44:17 +0300

    +fix

--- DesignPatterns_FactoryMethod/ViewController.swift
+++ DesignPatterns_FactoryMethod/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         createExercise(exName: .jumping)
+        createExercise(exName: .squarts)
         runExercise()
     }
 
